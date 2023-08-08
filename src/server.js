7d91ec01d387e44db88c6606f9b736c32d275c51
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-08-08 19:12:54 +0500

    userlar ro'yxatdan o'tish qismi yozildi

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { config } from './utils/config/index.js'
+import { usersRouter } from './routers/users.routes.js'
 
 const server = () => {
   try {
     const app = express()
     app.use(express.json())
+    app.use(usersRouter)
 
     app.listen(config.port, () => {
       console.log('Server is running')
