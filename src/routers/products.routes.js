3cb5129a3683561493071d24958c737931c13614
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-08-08 20:02:22 +0500

    productlar delete va get qilindi

--- src/routers/products.routes.js
+++ src/routers/products.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { postProducts } from "../controller/products.js";
+import {
+  postProducts,
+  getProducts,
+  deleteProducts,
+} from "../controller/products.js";
 
 export const productsRouter = Router();
 
 productsRouter.post("/products", postProducts);
+productsRouter.get("/products", getProducts);
+productsRouter.delete("/products/:id", deleteProducts);
