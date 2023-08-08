63fa1bc1c0a7e5c7da509a9437c329bc82a4dc95
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-08-08 18:09:50 +0500

    env configuration qilindi

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import { config } from './utils/config/index.js'
 
 const server = () => {
   try {
     const app = express()
     app.use(express.json())
 
-    app.listen(5000, () => {
+    app.listen(config.port, () => {
       console.log('Server is running')
     })
   } catch (error) {
